2fe04d06fb7f4b313261c6c92c6305fd47351876
Hang Qian <hq2124@columbia.edu>
2013-03-30 21:11:48 -0400

    fix

--- kernel/sched.c
+++ kernel/sched.c
@@ -5516,7 +5516,7 @@ sched_setscheduler_edf(struct task_struct *p, unsigned long deadline)
             printk(KERN_ALERT "3\n");
             p->sched_class->switched_to(rq, p, running);
             printk(KERN_ALERT "4\n");
-            __task_rq_unlock(rq)
+            __task_rq_unlock(rq);
             printk(KERN_ALERT "5\n");
             /* param = kmalloc(sizeof(struct sched_param),GFP_KERNEL); */
             /* param->sched_priority = p->prio; */
