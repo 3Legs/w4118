78574595fce99b22e0a91480c89edcb25878a67c
Hang Qian <hq2124@columbia.edu>
2013-03-30 20:04:17 -0400

    fix

--- kernel/sched_edf.c
+++ kernel/sched_edf.c
@@ -158,8 +158,9 @@ static struct task_struct *pick_next_task_edf(struct rq *rq)
 
     printk(KERN_ALERT "EDF has %lu tasks left \n", edf_rq->nr_running);
     se = pick_next_entity_edf(edf_rq);
-    if (se)
+    if (se){
         /* set_next_edf_entity(edf_rq, se); */
+    }
     else {
         printk(KERN_ALERT "Nothing left in EDF rq\n");
         return NULL;
