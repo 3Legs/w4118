79da7acfcaf4a24000ce3317d562d7cc4c87168f
Hang Qian <hq2124@columbia.edu>
2013-03-31 15:51:48 -0400

    Add task tick function

--- kernel/sched_edf.c
+++ kernel/sched_edf.c
@@ -209,6 +209,11 @@ check_preempt_curr_edf(struct rq *rq, struct task_struct *p, int sync)
 }
 
 static void 
+task_tick_edf (struct rq *rq, struct task_struct *curr, int queued)
+{
+    printk(KERN_ALERT "PID %d call Task-tick\n", curr->pid);
+}
+static void 
 set_curr_task_edf (struct rq *rq)
 {
     struct sched_edf_entity *se = &rq->curr->edf_se;
@@ -260,7 +265,7 @@ static const struct sched_class edf_sched_class = {
 #endif
 
 	.set_curr_task          = set_curr_task_edf,
-	/* .task_tick		= task_tick_edf, */
+	.task_tick		= task_tick_edf,
 	/* .task_new		= task_new_edf, */
 
 	/* .prio_changed		= prio_changed_edf, */
