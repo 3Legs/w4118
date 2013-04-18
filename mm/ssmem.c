9fa6e36cfce793f5c9fc55e9eba88af1a8c456b9
Hang Qian <hq2124@columbia.edu>
2013-04-18 19:45:37 -0400

    deal with anon_vma

--- mm/ssmem.c
+++ mm/ssmem.c
@@ -76,7 +76,7 @@ struct ssmem_struct {
 	struct ssmem_vm *vm_list; /* list of mappers */
 	struct anon_vma *rmap; /* reverse map */
 	struct list_head list;
-    struct mutex ssmem_vm_list_lock;
+	struct mutex ssmem_vm_list_lock;
 };
 
 /* function declare */
@@ -163,6 +163,7 @@ __ssmem_fault_master(struct vm_area_struct *vma,
 	inc_mm_counter(vma->vm_mm, anon_rss);
 	page_add_new_anon_rmap(page, vma, (unsigned long)addr);
 	set_pte_at(vma->vm_mm, (unsigned long)addr, page_table, entry);
+	vma->vm_mm->nr_ptes++;
 	pte_unmap_unlock(page_table, ptl);
 
 	return 0;
@@ -183,6 +184,7 @@ __ssmem_fault_slave(struct vm_area_struct *vma_s, struct vm_area_struct *vma_m,
 	}
 
 	set_pte_at(vma_s->vm_mm, (unsigned long)addr, pte_s, *pte_m);
+	vma_s->vm_mm->nr_ptes++;
 	pte_unmap_unlock(pte_m, ptl_m);
 	pte_unmap_unlock(pte_s, ptl_s);
 
@@ -195,7 +197,7 @@ static int ssmem_fault(struct vm_area_struct *vma, struct vm_fault *vmf)
 	int result;
 	struct ssmem_vm *cur, *next, *master_vm = NULL;
 
-	if (data->master == current->pid) {
+	if (SSMEM_MASTER(data) == current->pid) {
 		result = __ssmem_fault_master(vma, data, vmf->virtual_address, MASTER_CALL);
 	} else {
 		mutex_lock(&data->ssmem_vm_list_lock);
@@ -242,6 +244,7 @@ __copy_page_table(struct vm_area_struct *source_vma,
 			pte_target = get_locked_pte(target_vma->vm_mm, target_start + offset, &ptl_target);
 			set_pte_at(target_vma->vm_mm, target_start + offset, pte_target, *pte_source);
 			page = pte_page(*pte_source);
+			target_vma->vm_mm->nr_ptes++;
 			get_page(page);
 			pte_unmap_unlock(pte_target, ptl_target);
 		}
@@ -329,6 +332,8 @@ __detach_ssmem_vma_to_be_unmapped(struct mm_struct *mm,
 
 	rb_erase(&vma->vm_rb, &mm->mm_rb);
 	mm->map_count--;
+	
+	anon_vma_unlink(vma);
 
 	next = vma->vm_next;
 	prev->vm_next = next;
