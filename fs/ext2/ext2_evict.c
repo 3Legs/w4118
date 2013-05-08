477ff608791c4c927f46664b1553ed70f32f1efe
Hang Qian <hq2124@columbia.edu>
2013-05-07 22:23:57 -0400

    indent fix

--- fs/ext2/ext2_evict.c
+++ fs/ext2/ext2_evict.c
@@ -38,10 +38,10 @@
 #define SEND_SIZE 4096
 
 enum clfs_status {
-	CLFS_OK = 0,            /* Success */
-	CLFS_INVAL = EINVAL,    /* Invalid address */
-	CLFS_ACCESS = EACCES,   /* Could not read/write file */
-	CLFS_ERROR             /* Other errors */
+	CLFS_OK = 0,		/* Success */
+	CLFS_INVAL = EINVAL,	/* Invalid address */
+	CLFS_ACCESS = EACCES,	/* Could not read/write file */
+	CLFS_ERROR		/* Other errors */
 };
 
 enum clfs_type {
@@ -66,44 +66,50 @@ struct evicted {
 
 DEFINE_MUTEX(evict_mutex);
 
-static inline void __prepare_msghdr(struct msghdr *hdr, struct iovec * iov, void *data, size_t len, int flags) {
+static inline void __prepare_msghdr(struct msghdr *hdr, struct iovec *iov,
+				    void *data, size_t len, int flags)
+{
 
 	iov->iov_base = data;
 	iov->iov_len = len;
-	
+
 	hdr->msg_name = NULL;
 	hdr->msg_namelen = 0;
 	hdr->msg_control = NULL;
 	hdr->msg_controllen = 0;
 	hdr->msg_flags = flags;
 	hdr->msg_iov = iov;
-	hdr->msg_iovlen  = 1;
+	hdr->msg_iovlen = 1;
 }
 
-static inline void __prepare_addr(struct sockaddr_in *addr, struct inode*i) {
+static inline void __prepare_addr(struct sockaddr_in *addr, struct inode *i)
+{
 	addr->sin_family = AF_INET;
-	addr->sin_port = htons(((struct ext2_sb_info *)i->i_sb->s_fs_info)->port);
-	addr->sin_addr.s_addr = in_aton(((struct ext2_sb_info *)i->i_sb->s_fs_info)->ip);
+	addr->sin_port =
+	    htons(((struct ext2_sb_info *)i->i_sb->s_fs_info)->port);
+	addr->sin_addr.s_addr =
+	    in_aton(((struct ext2_sb_info *)i->i_sb->s_fs_info)->ip);
 
-	printk(KERN_ALERT "IP: %s, port: %d\n", ((struct ext2_sb_info *)i->i_sb->s_fs_info)->ip, ntohs(addr->sin_port));
+	printk(KERN_ALERT "IP: %s, port: %d\n",
+	       ((struct ext2_sb_info *)i->i_sb->s_fs_info)->ip,
+	       ntohs(addr->sin_port));
 
 }
 
 static inline int __connect_socket(struct socket *socket,
-				   struct sockaddr_in* server_addr,
-				   struct inode* i)
+				   struct sockaddr_in *server_addr,
+				   struct inode *i)
 {
 	int r = 0;
-	r = socket->ops->connect(socket, 
-				 (struct sockaddr *) server_addr,
-				 sizeof(struct sockaddr), 0);	
+	r = socket->ops->connect(socket,
+				 (struct sockaddr *)server_addr,
+				 sizeof(struct sockaddr), 0);
 	return r;
 }
 
-static void __send_request(struct socket *socket, 
-			  struct sockaddr_in* addr, 
-			  struct inode *i_node,
-			  enum clfs_type type)
+static void __send_request(struct socket *socket,
+			   struct sockaddr_in *addr,
+			   struct inode *i_node, enum clfs_type type)
 {
 	struct clfs_req *req;
 	struct msghdr hdr;
@@ -115,36 +121,41 @@ static void __send_request(struct socket *socket,
 	req->inode = i_node->i_ino;
 	req->size = i_node->i_size;
 
-	
-	__prepare_msghdr(&hdr, &iov, (void *) req, sizeof(struct clfs_req), MSG_DONTWAIT);
-	printk(KERN_ALERT "Req size: %d, Send size %d\n", hdr.msg_iov->iov_len, sizeof(struct clfs_req));
+	__prepare_msghdr(&hdr, &iov, (void *)req, sizeof(struct clfs_req),
+			 MSG_DONTWAIT);
+	printk(KERN_ALERT "Req size: %d, Send size %d\n", hdr.msg_iov->iov_len,
+	       sizeof(struct clfs_req));
 	oldmm = get_fs();
 	set_fs(KERNEL_DS);
 	sock_sendmsg(socket, &hdr, sizeof(struct clfs_req));
 	set_fs(oldmm);
 }
 
-static void __send_response(struct socket *socket, enum clfs_status res) {
+static void __send_response(struct socket *socket, enum clfs_status res)
+{
 	enum clfs_status response = res;
 	struct msghdr hdr;
 	struct iovec iov;
 	mm_segment_t oldmm;
 
-	__prepare_msghdr(&hdr, &iov, (void *) &response, sizeof(enum clfs_status), MSG_DONTWAIT);
-	oldmm = get_fs(); 
+	__prepare_msghdr(&hdr, &iov, (void *)&response,
+			 sizeof(enum clfs_status), MSG_DONTWAIT);
+	oldmm = get_fs();
 	set_fs(KERNEL_DS);
 	sock_sendmsg(socket, &hdr, sizeof(enum clfs_status));
 	set_fs(oldmm);
 }
 
-static enum clfs_status __read_response(struct socket *socket) {
+static enum clfs_status __read_response(struct socket *socket)
+{
 	enum clfs_status response = CLFS_OK;
 	struct msghdr hdr;
 	struct iovec iov;
 	int len;
 	mm_segment_t oldmm;
 
-	__prepare_msghdr(&hdr, &iov, &response, sizeof(enum clfs_status), MSG_WAITALL);
+	__prepare_msghdr(&hdr, &iov, &response, sizeof(enum clfs_status),
+			 MSG_WAITALL);
 	oldmm = get_fs();
 	set_fs(KERNEL_DS);
 	len = sock_recvmsg(socket, &hdr, sizeof(enum clfs_status), MSG_WAITALL);
@@ -156,7 +167,6 @@ static enum clfs_status __read_response(struct socket *socket) {
 	return CLFS_ERROR;
 }
 
-
 /*
  * send file data through socket
  * 1. store the hash code of file data 
@@ -165,11 +175,10 @@ static enum clfs_status __read_response(struct socket *socket) {
  * 4. handle local data (delete page cache and reclaim blocks)
  */
 
-
 static int evict_page_cache_read(pgoff_t offset, struct inode *inode)
 {
 	struct address_space *mapping = inode->i_mapping;
-	struct page *page; 
+	struct page *page;
 	int ret;
 
 	do {
@@ -181,16 +190,18 @@ static int evict_page_cache_read(pgoff_t offset, struct inode *inode)
 		if (ret == 0)
 			ret = mapping->a_ops->readpage(NULL, page);
 		else if (ret == -EEXIST)
-			ret = 0; /* losing race to add is OK */
+			ret = 0;	/* losing race to add is OK */
 
 		page_cache_release(page);
 
 	} while (ret == AOP_TRUNCATED_PAGE);
-		
+
 	return ret;
 }
 
-static void __send_file_data_to_server(struct socket *socket, struct inode *i_node) {
+static void __send_file_data_to_server(struct socket *socket,
+				       struct inode *i_node)
+{
 	struct address_space *mapping = i_node->i_mapping;
 	/* unsigned long nr_pages = mapping->nrpages;  */
 	unsigned long size = i_node->i_size;
@@ -211,7 +222,7 @@ static void __send_file_data_to_server(struct socket *socket, struct inode *i_no
 			break;
 		}
 		++i;
-		
+
 		lock_page(page);
 		map = kmap(page);
 		memcpy(buf, map, SEND_SIZE);
@@ -225,13 +236,13 @@ static void __send_file_data_to_server(struct socket *socket, struct inode *i_no
 		remove_from_page_cache(page);
 		page_cache_release(page);
 		unlock_page(page);
-		
+
 		total_len += SEND_SIZE;
 		if (total_len >= size)
 			break;
 	}
 	kfree(buf);
-} 
+}
 
 /*
  * read file data from a socket
@@ -240,7 +251,9 @@ static void __send_file_data_to_server(struct socket *socket, struct inode *i_no
  * 3. write data to file
  */
 
-static enum clfs_status __read_file_data_from_server(struct socket *socket, struct inode *i_node) {
+static enum clfs_status __read_file_data_from_server(struct socket *socket,
+						     struct inode *i_node)
+{
 	struct address_space *mapping = i_node->i_mapping;
 	struct page *page;
 	struct msghdr hdr;
@@ -281,11 +294,11 @@ evict_retry:
 		}
 
 		map = kmap(page);
-		memcpy(map, buf, len); 
+		memcpy(map, buf, len);
 		mark_page_accessed(page);
 		kunmap(page);
 		unlock_page(page);
-		
+
 		total_len += len;
 		if (total_len >= size)
 			goto read_out;
@@ -296,7 +309,8 @@ read_out:
 	kfree(buf);
 
 	if (total_len != size) {
-		printk(KERN_ALERT "File length error %lu, %lu\n", total_len, size);
+		printk(KERN_ALERT "File length error %lu, %lu\n", total_len,
+		       size);
 		r = CLFS_ERROR;
 	} else {
 		printk(KERN_ALERT "File received success\n");
@@ -305,7 +319,8 @@ read_out:
 	return r;
 }
 
-int ext2_evict(struct inode *i_node) {
+int ext2_evict(struct inode *i_node)
+{
 
 	struct sockaddr_in *server_addr = NULL;
 	struct socket *socket;
@@ -337,18 +352,21 @@ int ext2_evict(struct inode *i_node) {
 
 	r = __read_response(socket);
 	if (r == CLFS_OK) {
-		printk(KERN_ALERT "Got OK response, start to evict file %lu\n", i_node->i_ino);
+		printk(KERN_ALERT "Got OK response, start to evict file %lu\n",
+		       i_node->i_ino);
 		__send_file_data_to_server(socket, i_node);
 		r = __read_response(socket);
 		if (r == CLFS_OK) {
-			printk(KERN_ALERT "Successfully evict file %lu\n", i_node->i_ino);			
+			printk(KERN_ALERT "Successfully evict file %lu\n",
+			       i_node->i_ino);
 			/* clean up local file here */
 			record_size = i_node->i_size;
 			i_size_write(i_node, 0);
 			ext2_truncate(i_node);
 			i_size_write(i_node, record_size);
 		} else {
-			printk(KERN_ALERT "Evict error %d on file %lu\n", r, i_node->i_ino);			
+			printk(KERN_ALERT "Evict error %d on file %lu\n", r,
+			       i_node->i_ino);
 		}
 	}
 
@@ -388,13 +406,15 @@ int ext2_fetch(struct inode *i_node)
 	__send_request(socket, server_addr, i_node, CLFS_GET);
 	r = __read_response(socket);
 	if (r == CLFS_OK) {
-		printk(KERN_ALERT "Got OK reponse, about to read file from server\n");
+		printk(KERN_ALERT
+		       "Got OK reponse, about to read file from server\n");
 		r = __read_file_data_from_server(socket, i_node);
 		if (r == CLFS_OK)
 			printk(KERN_ALERT "Read file success!\n");
-		__send_response(socket, (enum clfs_status) r);
+		__send_response(socket, (enum clfs_status)r);
 	} else if (r == CLFS_ACCESS) {
-		printk(KERN_ALERT "Can't access file %lu from server\n", i_node->i_ino);	
+		printk(KERN_ALERT "Can't access file %lu from server\n",
+		       i_node->i_ino);
 		r = -1;
 	}
 	if (socket)
@@ -448,18 +468,25 @@ int ext2_evict_fs(struct super_block *super)
 	struct ext2_sb_info *ext2_sup = super->s_fs_info;
 	struct ext2_super_block *ext2_es = ext2_sup->s_es;
 
-	struct timespec *scan_time = kmalloc(sizeof(struct timespec), GFP_KERNEL);
-	struct timespec *set_time = kmalloc(sizeof(struct timespec), GFP_KERNEL);
-	struct timespec *current_time = kmalloc(sizeof(struct timespec), GFP_KERNEL);
-	struct clock_hand *clockhand = kmalloc(sizeof(struct clock_hand), GFP_KERNEL);
-	struct evicted *set_evicted = kmalloc(sizeof(struct evicted), GFP_KERNEL);
-	struct evicted *scan_evicted = kmalloc(sizeof(struct evicted), GFP_KERNEL);
+	struct timespec *scan_time =
+	    kmalloc(sizeof(struct timespec), GFP_KERNEL);
+	struct timespec *set_time =
+	    kmalloc(sizeof(struct timespec), GFP_KERNEL);
+	struct timespec *current_time =
+	    kmalloc(sizeof(struct timespec), GFP_KERNEL);
+	struct clock_hand *clockhand =
+	    kmalloc(sizeof(struct clock_hand), GFP_KERNEL);
+	struct evicted *set_evicted =
+	    kmalloc(sizeof(struct evicted), GFP_KERNEL);
+	struct evicted *scan_evicted =
+	    kmalloc(sizeof(struct evicted), GFP_KERNEL);
 
-	int used_blocks = ext2_es->s_blocks_count - ext2_count_free_blocks(super);
+	int used_blocks =
+	    ext2_es->s_blocks_count - ext2_count_free_blocks(super);
 	int total_blocks = ext2_es->s_blocks_count;
 	int utility = (used_blocks * 1000) / total_blocks;
-	long min_inode_number = (long) EXT2_FIRST_INO(super);
-	long max_inode_number = (long) le32_to_cpu(ext2_es->s_inodes_count);
+	long min_inode_number = (long)EXT2_FIRST_INO(super);
+	long max_inode_number = (long)le32_to_cpu(ext2_es->s_inodes_count);
 	long current_inode;
 	int res;
 
@@ -480,10 +507,12 @@ int ext2_evict_fs(struct super_block *super)
 		clockhand->hand = min_inode_number;
 		current_inode = min_inode_number;
 
-		printk(KERN_ALERT "min: %lu max: %lu\n", min_inode_number, max_inode_number);
+		printk(KERN_ALERT "min: %lu max: %lu\n", min_inode_number,
+		       max_inode_number);
 
 		res = ext2_xattr_set(root_inode, EXT2_XATTR_INDEX_TRUSTED,
-				     "clockhand", clockhand, sizeof(struct clock_hand), 0);
+				     "clockhand", clockhand,
+				     sizeof(struct clock_hand), 0);
 		mutex_unlock(&root_inode->i_mutex);
 		if (res < 0) {
 			printk(KERN_ALERT "Error in ext2_xattr_set.\n");
@@ -499,14 +528,16 @@ int ext2_evict_fs(struct super_block *super)
 		node = evict_ext2_iget(super, current_inode);
 		if ((void *)node == (void *)(-ESTALE) || node == NULL)
 			goto continue_loop_with_no_lock;
-		
-		if (!S_ISREG(node->i_mode) || atomic_read(&node->i_writecount) > 0)
+
+		if (!S_ISREG(node->i_mode)
+		    || atomic_read(&node->i_writecount) > 0)
 			goto continue_loop_with_no_lock;
 
 		mutex_lock(&node->i_mutex);
 		getnstimeofday(current_time);
 		res = ext2_xattr_get(node, EXT2_XATTR_INDEX_TRUSTED,
-				     "scantime", scan_time, sizeof(struct timespec));
+				     "scantime", scan_time,
+				     sizeof(struct timespec));
 
 		if (res < 0)
 			scan_time->tv_sec = scan_time->tv_nsec = 0;
@@ -515,7 +546,8 @@ int ext2_evict_fs(struct super_block *super)
 		set_time->tv_nsec = current_time->tv_nsec;
 
 		res = ext2_xattr_set(node, EXT2_XATTR_INDEX_TRUSTED,
-				     "scantime", set_time, sizeof(struct timespec), 0);
+				     "scantime", set_time,
+				     sizeof(struct timespec), 0);
 		if (res < 0) {
 			mutex_unlock(&node->i_mutex);
 			printk(KERN_ALERT "Error in ext2_xattr_set create.\n");
@@ -525,49 +557,65 @@ int ext2_evict_fs(struct super_block *super)
 		if (time_greater(scan_time, &node->i_atime)) {
 
 			res = ext2_xattr_get(node, EXT2_XATTR_INDEX_TRUSTED,
-					     "evicted", scan_evicted, sizeof(struct evicted));
+					     "evicted", scan_evicted,
+					     sizeof(struct evicted));
 
 			if (res >= 0 && scan_evicted->evicted == 1)
 				goto continue_loop;
 
-			printk(KERN_ALERT "Calling ext2_evict, i_ino: %lu, i_writecount: %d free_blocks: %lu\n", 
-				node->i_ino, atomic_read(&node->i_writecount), ext2_count_free_blocks(super));
+			printk(KERN_ALERT
+			       "Calling ext2_evict, i_ino: %lu, i_writecount: %d free_blocks: %lu\n",
+			       node->i_ino, atomic_read(&node->i_writecount),
+			       ext2_count_free_blocks(super));
 
 			res = ext2_evict(node);
 			if (!res) {
 				set_evicted->evicted = 1;
-				res = ext2_xattr_set(node, EXT2_XATTR_INDEX_TRUSTED,
-						     "evicted", set_evicted, sizeof(struct evicted), 0);
+				res =
+				    ext2_xattr_set(node,
+						   EXT2_XATTR_INDEX_TRUSTED,
+						   "evicted", set_evicted,
+						   sizeof(struct evicted), 0);
 				if (res < 0) {
 					mutex_unlock(&node->i_mutex);
-					printk(KERN_ALERT "Error in ext2_xattr_set.\n");
+					printk(KERN_ALERT
+					       "Error in ext2_xattr_set.\n");
 					goto out;
 				}
 			}
 
 			ext2_sup = super->s_fs_info;
 			ext2_es = ext2_sup->s_es;
-			used_blocks = ext2_es->s_blocks_count - ext2_count_free_blocks(super);
+			used_blocks =
+			    ext2_es->s_blocks_count -
+			    ext2_count_free_blocks(super);
 			/* used_blocks = ext2_es->s_blocks_count - super */
 			utility = (used_blocks * 1000) / total_blocks;
 
-			printk(KERN_ALERT "utility after ext2_evict: %d free_blocks: %lu\n", utility, ext2_count_free_blocks(super));
+			printk(KERN_ALERT
+			       "utility after ext2_evict: %d free_blocks: %lu\n",
+			       utility, ext2_count_free_blocks(super));
 
 			if (utility < 10 * ext2_sup->evict) {
 				clockhand->hand = current_inode;
-				res = ext2_xattr_set(root_inode, EXT2_XATTR_INDEX_TRUSTED,
-						     "clockhand", clockhand, sizeof(struct clock_hand), 0);
+				res =
+				    ext2_xattr_set(root_inode,
+						   EXT2_XATTR_INDEX_TRUSTED,
+						   "clockhand", clockhand,
+						   sizeof(struct clock_hand),
+						   0);
 				if (res < 0)
-					printk(KERN_ALERT "Error in ext2_xattr_set replace.\n");
-				
+					printk(KERN_ALERT
+					       "Error in ext2_xattr_set replace.\n");
+
 				printk(KERN_ALERT "ext2_evict_fs return.\n");
 				mutex_unlock(&node->i_mutex);
 				goto out;
 			}
-		} 
-	continue_loop:
+		}
+continue_loop:
 		mutex_unlock(&node->i_mutex);
-	continue_loop_with_no_lock:
+continue_loop_with_no_lock:
 		++current_inode;
 		if (current_inode > max_inode_number)
 			current_inode = min_inode_number;
